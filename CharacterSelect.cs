bf60e9e4121fd1905bdd560faa3b7daa8efad12c
alandliu <52473359+alandliu@users.noreply.github.com>
2021-04-29 01:15:16 -0700

    Add files via upload

--- CharacterSelect.cs
+++ CharacterSelect.cs
@@ -17,6 +17,8 @@ public class CharacterSelect : MonoBehaviour
 
 
     public int turn = 0;
+    public int player1Index;
+    public int player2Index;
 
     // Start is called before the first frame update
     void Start()
@@ -44,16 +46,21 @@ public class CharacterSelect : MonoBehaviour
         if (turn == 0)
         {
             characterSS[index].SetActive(true);
+            player1Index = index;
             turn++;
         }
         else if (turn == 1) {
             characterSS2[index].SetActive(true);
             nextButton.SetActive(true);
+            turn++;
+            player2Index = index;
         }
     }
 
     public void toCombat()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        PlayerPrefs.SetInt("Player1", player1Index);
+        PlayerPrefs.SetInt("Player2", player2Index);
     }
 }
